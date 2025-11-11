3c1a6c288cb5d81400a94d9b2961d49b830211e2
MurilloNFaria <nascimentomurillo24@gmail.com>
2025-11-11 12:37:40 -0300

    fix: melhorias visuais no componente Home, adição de animações na página

--- frontend/GameHubFront/src/features/home/home.ts
+++ frontend/GameHubFront/src/features/home/home.ts
@@ -8,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class Home {
 
+  onMouseMove(event: MouseEvent) {
+    const element = event.currentTarget as HTMLElement;
+    const position = element.getBoundingClientRect();
+    const x = event.clientX - position.left;
+    const y = event.clientY - position.top;
+
+    const rotateX = ((y - position.height / 2) / position.height) * -20;
+    const rotateY = ((x - position.width / 2) / position.width) * 20;
+
+    element.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    element.style.textShadow = `${rotateX / 3}px ${rotateY / 3}px 10px rgba(0,0,0,0.6)`;
+  }
+
+  onMouseLeave(event: MouseEvent) {
+    const element = event.currentTarget as HTMLElement;
+    element.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  }
 }
