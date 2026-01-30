f3590dae131b93d814aeb1775236f5257ec948fe
MurilloNFaria <nascimentomurillo24@gmail.com>
2026-01-30 02:37:24 -0300

    feat: (parcial) melhoria visual na barra de navegação

--- frontend/GameHubFront/src/layout/nav/nav.ts
+++ frontend/GameHubFront/src/layout/nav/nav.ts
@@ -1,16 +1,21 @@
 import { Component, inject, input } from '@angular/core';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  router = inject(Router);
   loggedIn = input<boolean>();
+
+  state() {
+    return this.router.url !== '/welcome';
+  }
 
   logout() {
     this.accountService.logout();
